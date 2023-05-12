53b88174b9525e563648161cce5e22eafde6f373
Prince <princemehta516@gmail.com>
2023-05-12 11:55:45 +0530

    Update move, print func.

--- Board.java
+++ Board.java
@@ -7,19 +7,55 @@ public class Board {
     private int count;
 
     public final static int PLAYER_1_WINS = 1;
-    public final static int PLAYER_2_WINS = 1;
-    public final static int DRAW = 1;
-    public final static int INCOMPLETE = 1;
-    public final static int INVALID = 1;
+    public final static int PLAYER_2_WINS = 2;
+    public final static int DRAW = 3;
+    public final static int INCOMPLETE = 4;
+    public final static int INVALID = 5;
 
     public Board(char p1Symbol, char p2Symbol){
         board = new char[boardsize][boardsize];
         for(int i=0;i<boardsize;i++){
             for(int j=0;j<boardsize;j++){
-                board[i][j] = ' ';
+                board[i][j] = '-';
             }
         }
         this.p1Symbol = p1Symbol;
         this.p2Symbol = p2Symbol;
+    }
+    public int move(char symbol, int x, int y){
+        //check if move is valid or not
+        if(x<0 || x>=boardsize || y<0 || y>=boardsize || board[x][y]!='-'){
+            return INVALID;
+        }
+        board[x][y] = symbol;
+        count++;
+
+        if(board[x][0]!='-' && board[x][0] == board[x][1] && board[x][0] == board[x][2]){
+            return symbol==p1Symbol ? PLAYER_1_WINS : PLAYER_2_WINS;
+        }
+        if(board[0][y]!='-' && board[0][y] == board[1][y] && board[0][y]==board[2][y]){
+            return symbol==p1Symbol ? PLAYER_1_WINS : PLAYER_1_WINS;
+        }
+        if(board[0][0]!='-' && board[0][0] == board[1][1] && board[0][0] == board[2][2]){
+            return symbol==p1Symbol ? PLAYER_1_WINS : PLAYER_2_WINS;
+        }
+        if(board[0][2]!='-' && board[0][2]==board[1][1] && board[0][2]==board[2][0]){
+            return symbol==p1Symbol ? PLAYER_1_WINS : PLAYER_2_WINS;
+        }
+        if(count==boardsize*boardsize)
+            return DRAW;
+        return INCOMPLETE;
+    }
+    public void print(){
+        System.out.println("---------------");
+        for(int i=0;i<boardsize;i++){
+            for(int j=0;j<boardsize;j++){
+                System.out.print("| "+board[i][j]+" |");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("---------------");
+
     }
 }
--- TicTacToe.java
+++ TicTacToe.java
@@ -4,6 +4,11 @@ public class TicTacToe {
     private Player player1, player2;
     private Board board;
 
+    public static void main(String[] args){
+        TicTacToe t = new TicTacToe();
+        t.startGame();
+    }
+
     public void startGame(){
         Scanner sc = new Scanner(System.in);
         //player input
@@ -13,6 +18,47 @@ public class TicTacToe {
             System.out.println("Symbol already taken !! Please take another one !!");
             char s = sc.next().charAt(0);
             player2.setSymbol(s);
+        }
+        //create board
+        board = new Board(player1.getSymbol(), player2.getSymbol());
+        //conduct the game
+        boolean player1Turn=true;
+        int status = Board.INCOMPLETE;
+        while(status == Board.INCOMPLETE || status==Board.INVALID){
+            if(player1Turn){
+                System.out.println("Player 1 - "+player1.getName()+ "'s turn");
+                System.out.println("Enter x : ");
+                int x = sc.nextInt();
+                System.out.println("Enter y : ");
+                int y = sc.nextInt();
+                status = board.move(player1.getSymbol(),x,y);
+                if(status!=Board.INVALID){
+                    player1Turn=false;
+                    board.print();
+                }else{
+                    System.out.println("Invalid move !! Try Again");
+                }
+            }else{
+                System.out.println("Player 2 - "+player2.getName()+ "'s turn");
+                System.out.println("Enter x : ");
+                int x = sc.nextInt();
+                System.out.println("Enter y : ");
+                int y = sc.nextInt();
+                status = board.move(player2.getSymbol(),x,y);
+                if(status!=Board.INVALID){
+                    player1Turn=true;
+                    board.print();
+                }else{
+                    System.out.println("Invalid move !! Try again !!");
+                }
+            }
+        }
+        if(status==Board.PLAYER_1_WINS){
+            System.out.println("Player 1 - "+player1.getName()+" wins");
+        }else if(status==Board.PLAYER_2_WINS){
+            System.out.println("PLayer 2 - "+player2.getName()+" wins");
+        }else{
+            System.out.println("Match Draw");
         }
     }
     private Player takeInput(int num){
